f2d6301614613455a0e3825abc218ae9d2871b69
ktotah <ket2137@columbia.edu>
2024-06-10 11:57:25 -0400

    updated the apollo client setup in client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,45 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Navbar from './components/Navbar';
+// Determine the correct URI for the GraphQL endpoint
+const httpLink = createHttpLink({
+  uri:
+    window.location.hostname === "localhost"
+      ? "http://localhost:3002/graphql"
+      : "https://your-actual-production-backend.com/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
