61205673f40e113a16b96aca1f3b7013a755fd48
ktotah <ket2137@columbia.edu>
2024-06-12 02:17:03 -0400

    Attempting to configure for Render deployment

--- server/server.js
+++ server/server.js
@@ -47,17 +47,11 @@ const startApolloServer = async () => {
   });
 
   // Serve static files from the React app
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
-  } else {
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-    });
-  }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
